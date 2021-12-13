b8db54ddda1ceabb2a4d4d2c4c7e19604c11f89b
Jayant Prakash <jayant.11snh@gmail.com>
2021-12-13 14:55:28 -0800

    Adding Cors Blocker

--- Backend/VaccinationManagementSystem/src/main/java/com/example/VaccinationManagementSystem/VaccinationManagementSystemApplication.java
+++ Backend/VaccinationManagementSystem/src/main/java/com/example/VaccinationManagementSystem/VaccinationManagementSystemApplication.java
@@ -7,20 +7,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class VaccinationManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VaccinationManagementSystemApplication.class, args);
     }
     @Bean
-    public WebMvcConfigurer configure() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:3000");
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
