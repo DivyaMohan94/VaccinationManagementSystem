57ba19bc2d4014d12b64776ed485dbe27d3c00e6
Lasya Bheemendra Nalini <59815536+lasyabheemendra@users.noreply.github.com>
2021-12-16 16:28:56 -0800

    image

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -40,24 +40,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
   },
-  image: {
-    //backgroundImage: `url(${image})`,
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
   paper: {
     margin: theme.spacing(8, 4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
